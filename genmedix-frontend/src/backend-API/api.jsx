35fe574ac3a691db85f3fbb394d3b719bb262cc6
Samuel <samuelajala01@gmail.com>
2024-06-18 20:52:54 +0100

    chore: edit baseurl comment

--- genmedix-frontend/src/backend-API/api.jsx
+++ genmedix-frontend/src/backend-API/api.jsx
@@ -1,4 +1,4 @@
-// this is the remote host url can be change to local host url
+// this is the local host url can be change to remote host url
 const baseurl = 'http://127.0.0.1:8000' 
 
 export default baseurl;
